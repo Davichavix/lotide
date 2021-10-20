d58d5e5b10bd8c8a1f262027cac3ccca09429d0d
David Chan <dchan902@gmail.com>
2021-10-20 17:35:03 -0600

    assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🟢 Assertation Passed:, ${actual} " === " ${expected}`);
+    console.log(`🟢 Assertation Passed:, ${actual} === ${expected}`);
   } else {
-    console.log(`🔴 Assertation Passed:, ${actual} " !== " ${expected}`);
+    console.log(`🔴 Assertation Passed:, ${actual} !== ${expected}`);
   }
 };
 
