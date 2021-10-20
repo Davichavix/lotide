9d6c526641f170bf5af2ad4b0479333fa6211e9e
David Chan <dchan902@gmail.com>
2021-10-19 23:02:14 -0600

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢 " + "Assertation Passed:", actual + " === " + expected);
+  } else {
+    console.log("🔴 " + "Assertation Failed:", actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
