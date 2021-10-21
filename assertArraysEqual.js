174475846744ef9d25590f842d6b70f45b125ca7
David Chan <dchan902@gmail.com>
2021-10-20 21:45:59 -0600

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(firstArr, secArr) {
+  if (firstArr.length !== secArr.length) return false;
+  for (let i = 0; i < secArr.length; i++) {
+    if (firstArr[i] !== secArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢 Assertation Passed:, ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertation Passed:, ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual(["1", "2", "3"], ["1", 1, "3"]);
