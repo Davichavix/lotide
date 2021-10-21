35d245cefc81588e76d72761dfa7e19c41c56dea
David Chan <dchan902@gmail.com>
2021-10-20 21:39:30 -0600

    eqArray.js

---
+++ eqArray.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertation Passed:, ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertation Passed:, ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArr, secArr) {
+  if (firstArr.length !== secArr.length) return false;
+  for (let i = 0; i < secArr.length; i++) {
+    if (firstArr[i] !== secArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
