2e8b8838eee7345eaae2450c2e395fb8e3524885
David Chan <dchan902@gmail.com>
2021-10-27 21:56:45 -0600

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,14 @@
+
+const assertEqual = require("../assertEqual");
+
+const eqArrays = function(firstArr, secArr) {
+  if (firstArr.length !== secArr.length) return false;
+  for (let i = 0; i < secArr.length; i++) {
+    if (!assertEqual(firstArr[i], secArr[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = eqArrays;
