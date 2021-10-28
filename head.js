b6daed3e05007dec2bbcc72ad0314b7efdae22df
David Chan <dchan902@gmail.com>
2021-10-27 21:26:53 -0600

    Refactor

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertation Passed:, ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertation Failed:, ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(testarray){
   return testarray[0];
-}
+};
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([[1,2],2,3]));
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+
+console.log(assertEqual("lighthouse labs", "lighthouse labs"))
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
