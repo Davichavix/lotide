2da44521fbc8f8f372597bc6f3f4a9cb7822245d
David Chan <dchan902@gmail.com>
2021-10-28 19:54:17 -0600

    Refactor

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,9 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
-assertArraysEqual(middle([1 ,2, 3, 4, 5]), [3]);
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,9 @@
+const assert = require('chai').assert;
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("return '[Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
