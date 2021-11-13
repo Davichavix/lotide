39b1a1531e18c0e173d01f717318c0ef12e37757
David Chan <dchan902@gmail.com>
2021-11-12 17:38:40 -0700

    added exported functions

--- index.js
+++ index.js
@@ -4,15 +4,35 @@ const middle = require("./middle");
 const assertArraysEqual = require("./assertArraysEqual");
 const assertObjectsEqual = require("./assertObjectsEqual");
 const flatten = require("./flatten");
+const assertEqual = require("./assertEqual");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+const countLetters = require("./countLetters");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const eqObjects = require("./eqObjects");
+const eqArrays = require("./eqArrays");
+const countOnly = require("./countOnly");
+const letterPositions = require("./letterPositions")
 
 module.exports = {
   assertArraysEqual,
-  head,
-  tail,
-  middle,
   assertEqual,
   assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
   flatten,
-
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without,
 };
 
